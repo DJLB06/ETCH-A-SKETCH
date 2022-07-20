4c059a38dc455837a8b770890b746097bdf1efa3
Daniel Longmuir <djlongmuir@gmail.com>
2022-07-19 18:20:42 -0600

    fix grid in place

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
 const container = document.querySelector(".container");
 
-
 function buildGrid(){
-    for(let i=0;i<256;i++){
+    for(let i=0;i<64;i++){
         let cell = document.createElement("div");
-        cell.innerText = "test";
         cell.className ="test1";
         container.appendChild(cell);
     }
